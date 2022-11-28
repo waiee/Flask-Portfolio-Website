1f313ebf042d7c335d7898694428f230ddf8ce07
waiee <waiee_z@yahoo.com>
2022-11-28 16:33:10 +0800

    link html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app=Flask(__name__)
 
-# @app.route('/')
-# def home():
-#     return render_template("home.html")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/about/')
 def about():
