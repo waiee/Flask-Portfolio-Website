4b95d37c89920085c9f80f69c555fb80407f8f04
waiee <waiee_z@yahoo.com>
2022-11-28 16:16:23 +0800

    test

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("home.html")
+# @app.route('/')
+# def home():
+#     return render_template("home.html")
 
 @app.route('/about/')
 def about():
